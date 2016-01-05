6729717bc943b4f23a2be1fad44e85946aef5644
Charles <738787105@qq.com>
2016-01-05 10:12:22 +0800

    修改确定A类型的1.5版本

--- FormKiwiCrawler/Resource.Designer.cs
+++ FormKiwiCrawler/Resource.Designer.cs
@@ -59,15 +59,5 @@ namespace FormKiwiCrawler {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
-        /// </summary>
-        internal static System.Drawing.Bitmap busy {
-            get {
-                object obj = ResourceManager.GetObject("busy", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
